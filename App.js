096787a617585355723850bb3a1357290bf74d22
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-04-12 15:19:15 +0530

    upload

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MobileListScreen from './screens/MobileListScreen.js';
+import MobileDetailScreen from './screens/MobileDetailScreen.js';
+import { MobileListProvider } from './components/MobileListContext.js';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MobileListProvider>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="MobileListScreen">
+          <Drawer.Screen name="ListScreen" component={MobileListScreen} />
+          <Drawer.Screen name="DetailScreen" component={MobileDetailScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </MobileListProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
